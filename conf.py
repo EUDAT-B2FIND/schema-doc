83bd43bb1f1c50076191cba778898e34d33ec19f
Lenzma <41636532+Lenzma@users.noreply.github.com>
2021-01-22 16:53:47 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Claudia Martens, Anna-Lena Flügel'
 author = 'Claudia Martens, Anna-Lena Flügel'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '2.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -49,7 +49,7 @@ master_doc = 'index'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
